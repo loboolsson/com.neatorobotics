e785b67210161d658417e36f76a724c79e30d2b0
Lobo Olsson <lobo.olsson@gmail.com>
2023-05-19 21:18:14 +0200

    Default debug to false for production env on start so people don't accidentally leave it on

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ class NeatoApp extends Homey.App {
 
   onInit() {
     this.log('Neato Robotics app is starting');
-    // Start inspector if we are in debug env
     if (process.env.DEBUG) {
+      // If we are in debug env start inspector and force the app debug setting to true
       inspector.open(9229, '0.0.0.0', false);
       this.homey.settings.set('debug', true);
       this.log('Neato Robotics app is in debug mode');
+    } else {
+      // Else force it to false so people don't accidentally keep it on for ever
+      this.homey.settings.set('debug', false);
     }
   }
 
